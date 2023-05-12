150bc2d57eeb61a6228b4791748542bcd1ce31b6
Isaac Oldwood <Isaac@mulberryrisk.com>
2023-05-12 21:39:23 +0100

    dynamic-drop-chance-v0-3

--- dynamic_drop_chance/game_files/setup.mjs
+++ dynamic_drop_chance/game_files/setup.mjs
@@ -1,5 +1,15 @@
-export async function setup({ onInterfaceReady }) {
-  onInterfaceReady(() => {
+export function setup(ctx) {
+  ctx.settings.section("Multipliers").add([
+    {
+      type: "number",
+      name: "max-kill-count-multiplier",
+      label: "Max Kill Count Multiplier",
+      default: 5,
+      min: 1,
+    },
+  ]);
+
+  const updateDropChances = () => {
     game.monsters.forEach(function (monster) {
       if (!monster) {
         return;
@@ -17,10 +27,23 @@ export async function setup({ onInterfaceReady }) {
 
         // 1%
         if (dropChance < 0.01) {
+          let maxUserKillCountMultiplier = ctx.settings
+            .section("Multipliers")
+            .get("max-kill-count-multiplier");
+          // Stop 0 kill count causing divide by inf
+          let killCountMultiplier = Math.max(
+            Math.ceil(killCount * dropChance),
+            1
+          );
           lootTable.drops[i].weight =
-            dropWeight * Math.ceil(killCount * dropChance);
+            dropWeight *
+            Math.min(killCountMultiplier, maxUserKillCountMultiplier);
         }
       }
     });
+  };
+
+  ctx.onInterfaceReady(() => {
+    updateDropChances();
   });
 }
