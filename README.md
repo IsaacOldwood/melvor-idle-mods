104bff84b6e55401d80b30c56017bf7af1bd53ff
Isaac Oldwood <Isaac@mulberryrisk.com>
2023-05-12 15:06:35 +0100

    dynamic-drop-chance-v0.1

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# melvor-idle-mods
-A collection of mods created for Melvor Idle
+# Melvor Idle Mods
+A collection of mods created for [Melvor Idle](https://melvoridle.com/)
+
+## What is Melvor Idle?
+From the [Wiki](https://wiki.melvoridle.com/w/Main_Page):
+>This is the Official Wiki for Melvor Idle, an Idle RPG inspired by RuneScape and created by Malcs (aka MrFrux), with help from Prat and Coolrox95.
+
+## Why are you creating mods?
+1. To shape my gameplay to be exactly how I want it
+2. To practice JavaScript
+
+# Current mods
+[Dynamic Drop Chances](https://mod.io/g/melvoridle/m/dynamic-drop-chances)
---
+++ dynamic_drop_chance/game_files/setup.mjs
@@ -0,0 +1,29 @@
+export async function setup({ onInterfaceReady }) {
+  onInterfaceReady(() => {
+    game.monsters.forEach(function (monster) {
+      if (!monster) {
+        return;
+      }
+      let lootTable = monster.lootTable;
+      let lootChance = monster.lootChance / 100;
+      let killCount = game.stats.monsterKillCount(monster);
+      if (!lootTable) {
+        return;
+      }
+      let totalWeight = lootTable.totalWeight;
+      for (let i = 0; i < lootTable.drops.length; i++) {
+        let dropWeight = lootTable.drops[i].weight;
+        let dropChance = (dropWeight / totalWeight) * lootChance;
+
+        // 1%
+        if (dropChance < 0.01) {
+          console.log(dropWeight);
+          console.log(dropChance);
+          console.log(killCount);
+          lootTable.drops[i].weight =
+            dropWeight * Math.ceil(killCount * dropChance);
+        }
+      }
+    });
+  });
+}
