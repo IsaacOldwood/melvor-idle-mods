077c92bc3deaf91d6b7e9beda7ebc20c722e56de
Isaac Oldwood <Isaac@mulberryrisk.com>
2024-06-27 09:44:33 +0100

    [MTiS] v0.4 update for v1.3

--- museum_tokens_in_shop/game_files/setup.mjs
+++ museum_tokens_in_shop/game_files/setup.mjs
@@ -3,11 +3,10 @@ export function setup(ctx) {
 
   ctx.onInterfaceReady(() => {
     museum_tokens.update(game.bank.getQty(game.items.getObjectByID("melvorAoD:Museum_Token")));
-    ui.create(museum_tokens, document.getElementById("shop-current-slayer-c").parentElement);
+    ui.create(museum_tokens, document.getElementById("shop-current-sc").parentElement.parentElement);
   });
 
   ctx.patch(Bank, "addItem").after((didAddItem, item, quantity, ...args) => {
-    console.log("5");
     museum_tokens.update(game.bank.getQty(game.items.getObjectByID("melvorAoD:Museum_Token")));
   });
 }
