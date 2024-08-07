9385ab78354eea51b6acf4cc67b89d96c2482911
Isaac Oldwood <Isaac@mulberryrisk.com>
2024-08-07 17:08:32 +0100

    [DDC] Create mod settings

--- dynamic-drop-chance/game_files/setup.mjs
+++ dynamic-drop-chance/game_files/setup.mjs
@@ -1,13 +1,18 @@
 // Run on startup
 export function setup(ctx) {
+  // Render mod settings
+  render_mod_settings(ctx);
+
   // After game data is loaded
   ctx.onModsLoaded(async (ctx) => {
     // Setup combat
+    console.log("[DDC] Saving default drop chances");
     game.monsters.forEach((monster) => {
       setupMonster(monster);
     });
 
     // Setup thieving
+    console.log("[DDC] Loading thieving data and saving default drop chances");
     game.thieving.areas.forEach((area) => {
       setupThievingArea(area);
     });
@@ -16,6 +21,7 @@ export function setup(ctx) {
   // Execute code after offline progress has been calculated and all in-game user interface elements have been created.
   ctx.onInterfaceReady(async (ctx) => {
     // Setup dungeon pets
+    console.log("[DDC] Saving default pet drop chances");
     game.dungeons.forEach((dungeon) => {
       setupDungeonPet(dungeon);
     });
@@ -136,4 +142,88 @@ function setupThievingNPC(npc) {
       drop.origWeight = drop.weight;
     }
   });
+}
+
+// #########################################################################################
+// MOD SETTINGS
+// #########################################################################################
+
+function render_mod_settings(ctx) {
+  ctx.settings.section("Combat").add([
+    {
+      type: "switch",
+      name: "completion-only",
+      label: "Apply multiplier to first drop only",
+      // onChange: (val) => {
+      //   ddc_params.completionOnlyInput = val;
+      //   game.monsters.forEach(updateCombatDropChances.bind(ddc_params));
+      // },
+      default: true,
+    },
+  ]);
+
+  ctx.settings.section("Combat").add([
+    {
+      type: "number",
+      name: "multiplier-threshold",
+      label: "Apply multiplier to drops with lower drop chance than:",
+      hint: "Decimal format (0.01 = 1%)",
+      onChange: (val) => {
+        if (val >= 1) {
+          return "Threshold must be in decimal format and less than 1";
+        } else if (val <= 0) {
+          return "Threshold must be in decimal format and greater than 0";
+        }
+      },
+      // onChange: (val) => {
+      //   ddc_params.multiplierThresholdInput = val;
+      //   game.monsters.forEach(updateCombatDropChances.bind(ddc_params));
+      // },
+      default: 0.01,
+    },
+  ]);
+
+  ctx.settings.section("Combat").add([
+    {
+      type: "number",
+      name: "max-multiplier",
+      label: "Maximum Multiplier",
+      hint: "Maximum multiplier that can be applied to drop chances",
+      // onChange: (val) => {
+      //   ddc_params.maxUserKillCountMultiplierInput = val;
+      //   game.monsters.forEach(updateCombatDropChances.bind(ddc_params));
+      // },
+      default: 5,
+      min: 1,
+    },
+  ]);
+
+  ctx.settings.section("Combat").add([
+    {
+      type: "number",
+      name: "loot-chance-multiplier",
+      label: "Loot Chance Multiplier",
+      hint: "If an enemy's loot chance is less than 100%, increase the loot chance (max 100%)",
+      // onChange: (val) => {
+      //   ddc_params.lootChanceMultiplierInput = val;
+      //   game.monsters.forEach(updateCombatDropChances.bind(ddc_params));
+      // },
+      default: 1,
+      min: 1,
+    },
+  ]);
+
+  ctx.settings.section("Pets").add([
+    {
+      type: "switch",
+      name: "pets-enabled",
+      label: "Enable Dynamic Drop Chances for pets",
+      // onChange: (val) => {
+      //   ddc_params.petEnabledInput = val;
+      //   game.dungeons.forEach(updateDungeonPetChance.bind(ddc_params));
+      //   game.slayerAreas.forEach(updateSlayerAreaPetChance.bind(ddc_params));
+      // },
+      default: true,
+    },
+  ]);
 }
